5fd4802e9f926d9e9d9e2d46efa16c579ce89d94
Cley Faye <cleyfaye@gmail.com>
2018-03-07 16:35:04 +0100

    [fix #3] Hide unpublished blog articles
     - Removed unpublished blog articles from the detailview

--- web/blog/urls.py
+++ web/blog/urls.py
@@ -1,14 +1,13 @@
 """blog URLs"""
 from django.conf.urls import url
-from django.views.generic.detail import DetailView
-from .models import Article
-from .views import ArticleListView
+from .views import (ArticleListView,
+                    ArticleDetailView)
 
 urlpatterns = [
     url(r'^$',
         ArticleListView.as_view(),
         name='list'),
     url(r'^(?P<pk>\d+)/$',
-        DetailView.as_view(model=Article),
+        ArticleDetailView.as_view(),
         name='article'),
 ]
